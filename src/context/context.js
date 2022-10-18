97010c9f06373303ccefad6491edc66e7935c9ff
unknown <millagoss19@gmail.com>
2022-10-18 14:16:57 +0300

    context_api created

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, {
+  useState,
+  useEffect,
+  useContext,
+  Children,
+  createContext,
+} from 'react';
 import mockUser from './mockData.js/mockUser';
 import mockRepos from './mockData.js/mockRepos';
 import mockFollowers from './mockData.js/mockFollowers';
 import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
+
+const GithubContext = createContext();
+
+export const GithubProvider = ({ children }) => {
+  const value = { ola: 'ola' };
+
+  return (
+    <GithubContext.Provider value={value}>{children}</GithubContext.Provider>
+  );
+};
+
+export const useGlobalGithubContext = () => {
+  return useContext(GithubContext);
+};
